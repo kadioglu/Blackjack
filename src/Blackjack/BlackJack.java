56ca2d31a6d83b3b454377b83c60f8f6c45866b4
miafriedman <mfriedm3@macalester.edu>
2020-04-23 12:41:02 -0700

    Hit while loop created

--- src/Blackjack/BlackJack.java
+++ src/Blackjack/BlackJack.java
@@ -2,14 +2,23 @@ package Blackjack;
 import comp127graphics.CanvasWindow;
 import comp127graphics.Line;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BlackJack {
     int runningtotal;
     Scanner scanner;
 
-    public static void main(String[]args){
-        int runningtotal = 0;
+    ArrayList<Card> playersCards;
+    ArrayList<Card> dealerCards;
+
+    public BlackJack() {
+        playersCards = new ArrayList<>();
+        dealerCards = new ArrayList<>();
+        runningtotal = 0;
+    }
+
+    private void runGame(){
         Deck mydeck = new Deck();
         Scanner scanner = new Scanner(System.in);
 
@@ -19,30 +28,47 @@ public class BlackJack {
         runningtotal += card1.getNumber();
         runningtotal+=card2.getNumber();
 
+        playersCards.add(card1);
+        playersCards.add(card2);
 
         System.out.println("Your first card is the " + card1.getName());
         System.out.println("Your next card is the " + card2.getName());
         System.out.println("Your current total is " + runningtotal + ". Would you like to hit or stay? (1 to hit, 2 to stay)");
         int response = scanner.nextInt();
 
-        if (response==1){
-           Card card3 = mydeck.drawRandomCard();
-        System.out.println("Your next card is the " + card3.getName());
-        System.out.println("Your current total is " + runningtotal + ". Would you like to hit or stay? (1 to hit, 2 to stay)");
-    }
-        else if (response==2){
-        System.out.println("Your final total is " + runningtotal + ".  Now it's the dealer's turn!");
-    }
+        while(response == 1){
+            hit(mydeck);
+            response = scanner.nextInt();
+        }
+        if (response==2){
+            System.out.println("Your final total is " + runningtotal + ".  Now it's the dealer's turn!");
+        }
         else{
-        System.out.println("Not a valid response!  Would you like to hit or stay? (1 to hit, 2 to stay)");
-        scanner.nextInt();
+            System.out.println("Not a valid response!  Would you like to hit or stay? (1 to hit, 2 to stay)");
+            scanner.nextInt();
+        }
     }
-}
 
+    public static void main(String[]args) {
+        BlackJack newgame = new BlackJack();
+        newgame.runGame();
+    }
 
-
+    private void hit(Deck mydeck){
+            Card card = mydeck.drawRandomCard();
+            System.out.println("Your next card is the " + card.getName());
+            playersCards.add(card);
+            int n = 0;
+            for(Card cardd: playersCards){
+                n += cardd.getNumber();
+            }
+            runningtotal+=card.getNumber();
+            System.out.println("Your current total is " + n + ". Would you like to hit or stay? (1 to hit, 2 to stay)");
 
         }
+    }
+
+
 
 
 
