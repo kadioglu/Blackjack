26e46b8ac54c4f7878dc3b8ed55cea1e952dfc5e
Katelyn <kbreen1234@gmail.com>
2020-04-22 21:12:50 -0500

    added print ln

--- src/Blackjack/BlackJack.java
+++ src/Blackjack/BlackJack.java
@@ -6,5 +6,6 @@ public class BlackJack {
     public static void main(String[] args) {
         System.out.println("this is a test");
         System.out.println("this is a test");
+        System.out.println("this is a test");
     }
 }
