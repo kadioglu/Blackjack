fc787030c6a5b5ac0d32f24d544378346b011a90
Katelyn <kbreen1234@gmail.com>
2020-05-05 22:42:55 -0500

    fixed package bug

---
+++ src/Blackjack/Card.java
@@ -0,0 +1,37 @@
+package Blackjack;
+
+/**
+ * This class creates a card object with a name and number
+ */
+
+public class Card {
+    private int number;
+    private String name;
+    public Card(int number, String name){
+        this.number = number;
+        this.name = name;
+    }
+
+    /**
+     * returns the value of the card object
+     */
+    public int getNumber(){
+        return this.number;
+    }
+
+    /**
+     * sets the value of the card object
+     */
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    /**
+     * returns the name (suit and value) of the card
+     */
+    public String getName(){
+        return this.name;
+    }
+
+
+}
